c6ec1f0b8cc6d434c83690d4f0111ec6ed398c90
Cory Barton <coryjbarton@gmail.com>
2018-08-16 11:39:47 -0600

    Created Class from scratch

---
+++ 12 - Programming in Swift 4 for Intermediates/Calsses and Objects/Calsses and Objects/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Car.swift
+//  Calsses and Objects
+//
+//  Created by Cory Barton on 8/16/18.
+//  Copyright © 2018 Cory Barton. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+    
+    var color = "Black"
+    var numberOfSeats = 5
+    
+}
---
+++ 12 - Programming in Swift 4 for Intermediates/Calsses and Objects/Calsses and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calsses and Objects
+//
+//  Created by Cory Barton on 8/9/18.
+//  Copyright © 2018 Cory Barton. All rights reserved.
+//
+
+import Foundation
+
+
+
